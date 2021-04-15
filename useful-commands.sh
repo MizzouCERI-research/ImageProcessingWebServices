e07e48fd155b6456f358f8fa09988c1da97735a9
wangso <wangso@missouri.edu>
2021-04-15 08:49:14 -0500

    initial upload

---
+++ useful-commands.sh
@@ -0,0 +1,21 @@
+# Server side: 
+
+docker run -p 5000:5000 Docker_image_ID
+
+docker logs -f container_ID
+
+# Command line to interact with Server 
+curl -X POST -H 'Content-Type: application/json' http://server_IP:port/setNextServer -d '{"server":"mylongerurllocalhost1"}'
+curl -X POST http://server_IP:port/init
+
+# or on machines that have HTTPS enabled:
+curl -X POST -H 'Content-Type: application/json' https://server_IP:port/setNextServer -d '{"server":"mylongerurllocalhost1"}'
+curl -X POST https://server_IP:port/init
+
+# client side:
+
+# to overwrite entrypoint command
+docker run -it --env server=server_IP:port --entrypoint bash Docker_image_ID
+
+# to use pre-defined entrypoint command
+docker run -it --env server=server_IP:port Docker_image_ID
